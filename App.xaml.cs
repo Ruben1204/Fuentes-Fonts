03efc3bd04dedb790cad4495d82f95a0b2e65856
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-08-03 19:29:42 -0500

    FirstCommit
    add implementation fonts

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RecursosMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace RecursosMAUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Lobster-Regular.ttf", "Lobster");
+                fonts.AddFont("Handjet-ExtraBold.ttf", "BoldHandjet");
+                fonts.AddFont("Handjet-Regular.ttf", "Reg-Handjet");
+                fonts.AddFont("ionicons.ttf", "icon");
+            });
+
+		return builder.Build();
+	}
+}
